dab065c377d6e868bb5b391b31bc6a61d2944063
Joilson Rabelo <joilson.rabello@gmail.com>
2018-09-08 22:44:30 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import MapContainer  from './map/MapContainer';
+
+class App extends Component {
+  render() {
+    return (
+      <MapContainer/>
+    );
+  }
+}
+
+export default App;
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const constants = {
+    googleKey : `AIzaSyDq6w8VWwqVWfiWYh7G6HjV-PEMbAnut2k`
+}
